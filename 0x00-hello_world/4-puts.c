6bcf966ef788303f801fb579391cdbc9107ef83d
Favour John <Favyjzy@gmail.com>
2023-02-16 15:29:49 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
- *
+/** 
+ * main - main block
  * Return: 0
  */
-init main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
